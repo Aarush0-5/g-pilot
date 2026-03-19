44f2b54b61df555ced7ab78f7d57cce8c176e32e
Aarush0-5 <singhaar0110@gmail.com>
2026-03-19 12:53:31 +0530

    Seo added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,11 @@
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: "IGNOU Pilot | Unofficial Utility Tool",
+  description: "A quick utility tools to get the official urls to IGNOU assignments, question papers and have a personalised date sheet sorter as well"
+}
 
 export default function RootLayout({
   children,
